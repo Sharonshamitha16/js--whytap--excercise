d937385ec540bcb74c33e558e624409ee8fffc4e
Sharonshamitha16 <sharonshamitha16@gmail.com>
2024-08-20 00:31:41 +0530

    objecthtml added

---
+++ JS-ression/script.js
@@ -0,0 +1,447 @@
+// // let budget = 180
+// // const Starlink = {
+// //     name: 'starLink',
+// //     Why: "Acess From Anywhere and Anytime",
+// //     Price: 99,
+// //     currency: '$',
+// //     Period: "Per Month",
+// //     Speed: "1Gbps",
+// //     unlimited: true,
+// //     data: "unlimited data",
+
+// //     avaialbleCountry : ['ant','af','can','uk'],
+// //     installationDevice:{
+// //         deviceName:"Starlink01010",
+// //         devietype:"router",
+// //         devicePrice:100,
+// //         devicespeed:'1 gbps',
+// //         CuurencyType: "dollar"
+// //     },
+
+// // };
+
+// // // console.log(Starlink)// blind method
+// // let InternetFee = Starlink.Price
+// // console.log(InternetFee, "InternetFee");
+// // let devicePrice =Starlink.installationDevice.devicePrice
+
+// // console.log(devicePrice, "devicePrice");
+// // let  StarlinkInstallationFee = InternetFee+devicePrice;
+// // console.log('$',StarlinkInstallationFee, "StarlinkInstallationFee")
+
+
+// let HannaBakes = {
+//     sweettreats: {
+//         ST_1: {
+//             product:'Muffins',
+//             amount: 6,
+//             quantity: 10,
+//             item_code: 'muf_002',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+//         ST_2: {
+//             product:'Cupcakes',
+//             amount: 5,
+//             quantity: 20,
+//             item_code: 'cup_003',
+//             cost_of_ingredients: 2,
+//             CurencyType: "dollar"
+//         },
+//         ST_3: {
+//             product:'Popsicles',
+//             amount: 10,
+//             quantity: 25,
+//             item_code: 'pop_004',
+//             cost_of_ingredients: 3,
+//             CurencyType: "dollar"
+//         },
+//         ST_4: {
+//             product:'CakePops',
+//             amount: 10,
+//             quantity: 20,
+//             item_code: 'cak_005',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+//         ST_5: {
+//             product:'Brownies',
+//             amount: 15,
+//             quantity: 25,
+//             item_code: 'brow_006',
+//             cost_of_ingredients: 2,
+//             CurencyType: "dollar"
+//         },
+//         ST_6: {
+//             product:'Blondies',
+//             amount: 10,
+//             quantity: 20,
+//             item_code: 'blon_007',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+//         ST_7: {
+//             product:'Macarons',
+//             amount: 12,
+//             quantity: 40,
+//             item_code: 'mac_008',
+//             cost_of_ingredients: 3,
+//             CurencyType: "dollar"
+//         },
+//         ST_8: {
+//             product:'Donuts',
+//             amount: 10,
+//             quantity: 30,
+//             item_code: 'don_010',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+
+//         ST_9: {
+//             product:'Cookies',
+//             amount: 5,
+//             quantity: 50,
+//             item_code: 'cook_011',
+//             cost_of_ingredients: 1,
+//         },
+//         ST_10: {
+//             product:'DessertBars',
+//             amount: 8,
+//             quantity: 30,
+//             item_code: 'des_012',
+//             cost_of_ingredients: 1,
+//         },
+//         ST_11: {
+//             product:'Tarts',
+//             amount: 20,
+//             quantity: 35,
+//             item_code: 'tarts_013',
+//             cost_of_ingredients: 3,
+//             CurencyType: "dollar"
+//         },
+//         ST_12: {
+//             product:'WhoopiePies',
+//             amount: 12,
+//             quantity: 50,
+//             item_code: 'whoop_014',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+//         ST_13: {
+//             product:'Fudge',
+//             amount: 7,
+//             quantity: 15,
+//             item_code: 'fud_015',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         }
+//     },
+//     cakes: {
+//         CS_1: {
+//             product:'Butter_Cake',
+//             amount: 15,
+//             quantity: 50,
+//             item_code: 'but_001',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+
+//         CS_2: {
+//             product:'Sponge_Cake',
+//             amount: 10,
+//             quantity: 60,
+//             item_code: 'spon_002',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+//         CS_3: {
+//             product:'RedVelvet_Cake',
+//             amount: 17,
+//             quantity: 55,
+//             item_code: 'red_003',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+//         CS_4: {
+//             product:'Cheesecake',
+//             amount: 12,
+//             quantity: 45,
+//             item_code: 'che_004',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+//         CS_5: {
+//             product:'Fruitcake',
+//             amount: 17,
+//             quantity: 30,
+//             item_code: 'fru_005',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+//         CS_6: {
+//             product:'BlackForest_Cake',
+//             amount: 8,
+//             quantity: 55,
+//             item_code: 'bla_006',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+//         CS_7: {
+//             product:'Mousse_Cake',
+//             amount: 20,
+//             quantity: 70,
+//             item_code: 'mo_007',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+//         CS_8: {
+//             product:'Birthday_Cake',
+//             amount: 10,
+//             quantity: 70,
+//             item_code: 'bir_008',
+//             cost_of_ingredients: 1,
+//             CurencyType: "dollar"
+//         },
+//     }
+
+// }
+// console.log("=================================")
+// console.log("Total products count")
+// console.log("=================================")
+
+// let total_sweettreats = HannaBakes.sweettreats.ST_1.quantity + 
+// HannaBakes.sweettreats.ST_2.quantity + 
+// HannaBakes.sweettreats.ST_3.quantity
+//     + HannaBakes.sweettreats.ST_4.quantity +
+//      HannaBakes.sweettreats.ST_5.quantity +
+//       HannaBakes.sweettreats.ST_6.quantity +
+//     HannaBakes.sweettreats.ST_7.quantity +
+//      HannaBakes.sweettreats.ST_8.quantity + 
+//      HannaBakes.sweettreats.ST_9.quantity +
+//     HannaBakes.sweettreats.ST_10.quantity + 
+//     HannaBakes.sweettreats.ST_11.quantity + 
+//     HannaBakes.sweettreats.ST_12.quantity+
+//     HannaBakes.sweettreats.ST_13.quantity
+
+// console.log(total_sweettreats, "are the total products of sweet treats from HANNA BAKES...!!!")
+
+// let total_cakes_from_hannabakes = HannaBakes.cakes.CS_8.quantity +
+//     HannaBakes.cakes.CS_1.quantity +
+//     HannaBakes.cakes.CS_2.quantity +
+//     HannaBakes.cakes.CS_3.quantity +
+//     HannaBakes.cakes.CS_4.quantity +
+//     HannaBakes.cakes.CS_5.quantity +
+//     HannaBakes.cakes.CS_6.quantity+
+//     HannaBakes.cakes.CS_7.quantity+
+//     HannaBakes.cakes.CS_8.quantity
+
+
+
+
+// console.log(total_cakes_from_hannabakes, "are the total products of cakes from HANNA BAKES...!!!")
+// console.log("=================================")
+
+
+// console.log("Menu list from Hanna Bakes")
+// console.log("=================================")
+
+// console.log(
+//     "*****Sweet treats******")
+// console.log("=================================")
+
+// console.log(
+
+//     "Blondies-$", HannaBakes.sweettreats. ST_6.amount,
+//     "Brownies-$", HannaBakes.sweettreats.ST_5.amount,
+//     "CakePops-$", HannaBakes.sweettreats.ST_4.amount,
+//     "Cookies-$", HannaBakes.sweettreats.ST_9.amount,
+//     "Cupcakes-$", HannaBakes.sweettreats.ST_2.amount,
+//     "DessertBars-$", HannaBakes.sweettreats.ST_10.amount,
+//     "Donuts-$", HannaBakes.sweettreats.ST_8.amount,
+//     "Fudge-$", HannaBakes.sweettreats.ST_13.amount,
+//     "Macarons-$", HannaBakes.sweettreats.ST_7.amount,
+//     "Muffins-$", HannaBakes.sweettreats.ST_1.amount,
+//     "Popsicles-$", HannaBakes.sweettreats.ST_3.amount,
+//     "Tarts-$", HannaBakes.sweettreats.ST_11.amount,
+//     "WhoopiePies-$", HannaBakes.sweettreats.ST_12.amount)
+
+// console.log("=================================")
+
+// console.log("***cakes***")
+// console.log("=================================")
+// console.log(
+//     "Birthday_Cake-$", HannaBakes.cakes.CS_8.amount,
+
+//     "BlackForest_Cake-$", HannaBakes.cakes.CS_6.amount,
+
+//     "Butter_Cake-$", HannaBakes.cakes.CS_1.amount,
+
+//     "Cheesecake-$", HannaBakes.cakes.CS_4.amount,
+
+//     "Fruitcake-$", HannaBakes.cakes.CS_5.amount,
+
+//     "Mousse_Cake-$", HannaBakes.cakes.CS_7.amount,
+
+//     "RedVelvet_Cake-$", HannaBakes.cakes.CS_3.amount,
+
+//     "Sponge_Cake-$", HannaBakes.cakes.CS_2.amount
+
+
+// )
+// console.log("=================================")
+
+// let customer ={}
+// let bill_item={
+//     sweettreats:{
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+//        ST_1:
+//        {
+//         product:'null',
+//         quantity:0,
+//         amount:0,
+//         item_code:0
+//        },
+
+
+//     },
+//     cakes:{
+
+//     }
+// }
+
+// let customer_item =prompt("Enter your item needed:")
+// let  customer_quantity =prompt("Enter the quantity of the item:")
+
+
+const bakery = {
+    name: "candytreats",
+    totalproduct: {
+        cake: { price: 20, piece: 10 },
+        gulabjamun: { price: 15, piece: 30 },
+        samosa: { price: 10, piece: 20 }
+    }
+}
+console.log(bakery.totalproduct.cake)
+console.log(bakery.totalproduct.gulabjamun)
+console.log(bakery.totalproduct.samosa)
+
+purchase = {
+    purchase1: {
+        persons: "1",
+        item: "cake",
+        piece: "2",
+        remaining: "8"
+    },
+    purchase2: {
+        persons: "4",
+        item: "gulabjamun",
+        piece: "10",
+        remaining: "10"
+
+    },
+    purchase3: {
+        persons: "6",
+        item: "samosa",
+        piece: "18",
+        remaining: "2"
+    }
+};
+
+
+bakery.totalproduct.cake.piece -= purchase.purchase1.piece;           // 10 - 2 = 8
+bakery.totalproduct.gulabjamun.piece -= purchase.purchase2.piece;     // 30 - 10 = 20
+bakery.totalproduct.samosa.piece -= purchase.purchase3.piece;         // 20 - 18 = 2
+//remaining items
+console.log("Remaining items in the bakery:")
+console.log("cake: piece-" + bakery.totalproduct.cake.piece + ", price-₹" + bakery.totalproduct.cake.price);
+console.log("gulabjamun: piece-" + bakery.totalproduct.gulabjamun.piece + ", price-₹" + bakery.totalproduct.gulabjamun.price);
+console.log("samosa: piece-" + bakery.totalproduct.samosa.piece + ", price-₹" + bakery.totalproduct.samosa.price)
+
+//purchase display
+
+console.log(purchase.purchase1)
+console.log(purchase.purchase2)
+console.log(purchase.purchase3)
